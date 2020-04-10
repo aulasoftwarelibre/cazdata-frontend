7796c34bf405f2712137b9631a0c60e29789c5fc
Francis Molina Prieto <francisjmp99@gmail.com>
2020-04-10 14:55:00 +0200

    Removed AppBar

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -2,13 +2,9 @@ import 'package:cazdata_frontend/ui/widget/index.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('CazData'),
-      ),
       body: DefaultTextStyle(
         style: TextStyle(
             inherit: true,
@@ -19,56 +15,58 @@ class Profile extends StatelessWidget {
             decorationColor: Colors.black,
             decorationStyle: TextDecorationStyle.solid,
             color: Colors.black),
-        child: Center(
-          child: Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 3,
-                      child: Text(
-                        'Cuenta',
-                        style: TextStyle(
-                            fontSize: 32, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          'Cuenta',
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: FlatButton(
-                        onPressed: () {
-                          /*...*/
-                        },
-                        color: Colors.red,
-                        textColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(18.0)),
-                        child: Text("Cerrar sesión",
-                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      Expanded(
+                        flex: 3,
+                        child: FlatButton(
+                          onPressed: () {
+                            /*...*/
+                          },
+                          color: Colors.red,
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(18.0)),
+                          child: Text("Cerrar sesión",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'Perfil',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
-                ProfileWidget(
-                    name: 'Adrián López',
-                    location: 'Córdoba, España',
-                    profilePic: 'AL'),
-                Divider(),
-                TextFieldWidget(header: 'Flying to', content: 'Flying to'),
-                TextFieldWidget(
-                    header: 'Nivel Cazador', content: 'Novato (10)'),
-                TextFieldWidget(
-                    header: 'Descripción',
-                    content:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
-              ],
+                    ],
+                  ),
+                  Text(
+                    'Perfil',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  ProfileWidget(
+                      name: 'Adrián López',
+                      location: 'Córdoba, España',
+                      profilePic: 'AL'),
+                  Divider(),
+                  TextFieldWidget(header: 'Flying to', content: 'Flying to'),
+                  TextFieldWidget(
+                      header: 'Nivel Cazador', content: 'Novato (10)'),
+                  TextFieldWidget(
+                      header: 'Descripción',
+                      content:
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
+                ],
+              ),
             ),
           ),
         ),
