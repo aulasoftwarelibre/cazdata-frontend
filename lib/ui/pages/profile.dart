db4241d31f2ac200bff4c2756c7cac2815e88c10
Adrian Lopez <30497404+AdrianLopezGue@users.noreply.github.com>
2020-07-26 12:39:41 +0200

    Remove share button in resumen journey and description in profile page (#45)

--- lib/ui/pages/profile.dart
+++ lib/ui/pages/profile.dart
@@ -78,10 +78,6 @@ class Profile extends StatelessWidget {
                       child: TextFieldWidget(
                           header: 'Nivel Cazador', content: 'Novato (10)'),
                     ),
-                    TextFieldWidget(
-                        header: 'Descripción',
-                        content:
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'),
                     SizedBox(height: 15.0),
                     Row(
                       children: <Widget>[
