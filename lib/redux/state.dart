a3913e46c61b9949c0b3cd7dd657c37c0267d00e
Sergio Gómez <sergio@uco.es>
2020-08-30 12:14:32 +0200

    Refactor journeys list widget (#61)
    * Use StreamBuilder instead redux
    
    * Remove journey-list feature

--- lib/redux/state.dart
+++ lib/redux/state.dart
@@ -1,6 +1,5 @@
 import 'package:cazdata_frontend/features/animal-list/state.dart';
 import 'package:cazdata_frontend/features/current-journey/state.dart';
-import 'package:cazdata_frontend/features/journey-list/state.dart';
 import 'package:cazdata_frontend/features/hunter/state.dart';
 import 'package:flutter/material.dart';
 
@@ -8,21 +7,19 @@ class AppState {
   final HunterState hunterState;
   final AnimalsListState animalsListState;
   final CurrentJourneyState currentJourneyState;
-  final HunterJourneysState hunterJourneysState;
 
   const AppState({
     @required this.hunterState,
     @required this.animalsListState,
     @required this.currentJourneyState,
-    @required this.hunterJourneysState,
   });
 
   factory AppState.initial() {
     return AppState(
-        hunterState: HunterState.initial(),
-        animalsListState: AnimalsListState.initial(),
-        currentJourneyState: CurrentJourneyState.initial(),
-        hunterJourneysState: HunterJourneysState.initial());
+      hunterState: HunterState.initial(),
+      animalsListState: AnimalsListState.initial(),
+      currentJourneyState: CurrentJourneyState.initial(),
+    );
   }
 
   AppState copyWith({
@@ -30,12 +27,11 @@ class AppState {
     bool userIsNew,
     AnimalsListState animalsListState,
     CurrentJourneyState currentJourneyState,
-    HunterJourneysState hunterJourneysState,
   }) {
     return new AppState(
-        hunterState: hunterState ?? this.hunterState,
-        animalsListState: animalsListState ?? this.animalsListState,
-        currentJourneyState: currentJourneyState ?? this.currentJourneyState,
-        hunterJourneysState: hunterJourneysState ?? this.hunterJourneysState);
+      hunterState: hunterState ?? this.hunterState,
+      animalsListState: animalsListState ?? this.animalsListState,
+      currentJourneyState: currentJourneyState ?? this.currentJourneyState,
+    );
   }
 }
