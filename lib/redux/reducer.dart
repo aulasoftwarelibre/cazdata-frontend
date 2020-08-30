d69da8288d85c57abe3f20905877ec8ea9de01ed
Sergio Gómez <sergio@uco.es>
2020-08-30 12:14:02 +0200

    Remove journey-list feature

--- lib/redux/reducer.dart
+++ lib/redux/reducer.dart
@@ -2,9 +2,7 @@ import 'package:cazdata_frontend/features/animal-list/reducers.dart';
 import 'package:cazdata_frontend/features/animal-list/state.dart';
 import 'package:cazdata_frontend/features/current-journey/reducers.dart';
 import 'package:cazdata_frontend/features/hunter/reducers.dart';
-import 'package:cazdata_frontend/features/journey-list/reducers.dart';
 import 'package:cazdata_frontend/features/current-journey/state.dart';
-import 'package:cazdata_frontend/features/journey-list/state.dart';
 import 'package:cazdata_frontend/features/hunter/state.dart';
 import 'package:cazdata_frontend/redux/index.dart';
 
@@ -12,11 +10,10 @@ AppState mainReducer(AppState state, dynamic action) {
   HunterState hunterState = reduceHunterState(state.hunterState, action);
   AnimalsListState animalsListState = reduceAnimalsListState(state.animalsListState, action);
   CurrentJourneyState currentJourneyState = reduceCurrentJourneysState(state.currentJourneyState, action);
-  HunterJourneysState hunterJourneysState = reduceHunterJourneysState(state.hunterJourneysState, action);
 
   return AppState(
-      hunterState: hunterState,
-      animalsListState: animalsListState,
-      currentJourneyState: currentJourneyState,
-      hunterJourneysState: hunterJourneysState);
+    hunterState: hunterState,
+    animalsListState: animalsListState,
+    currentJourneyState: currentJourneyState,
+  );
 }
